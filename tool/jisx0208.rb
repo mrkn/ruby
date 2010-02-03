52f313d6435e0495f4d422cecac50f493a003865
Kenta Murata <mrkn@mrkn.jp>
2010-02-04 00:26:24 +0900

    add kddi translation table generator and citrus source files

--- tool/jisx0208.rb
+++ tool/jisx0208.rb
@@ -6,16 +6,14 @@ module JISX0208
           raise ArgumentError, "out of the range of JIS X 0208: 0x#{sjis.to_s(16)}"
         end
         sjis_hi, sjis_lo = sjis >> 8, sjis & 0xFF
-        jis_hi, jis_lo = 0, sjis_lo
+        sjis_hi = (sjis_hi - ((sjis_hi <= 0x9F) ? 0x80 : 0xC0)) << 1
         if sjis_lo <= 0x9E
-          jis_lo -= 0x1F
-          jis_lo -= 1 if sjis_lo >= 0x80
-          jis_hi = 1
+          sjis_hi -= 1
+          sjis_lo -= (sjis_lo <= 0x7E) ? 0x3F : 0x40
         else
-          jis_lo -= 0x7E
+          sjis_lo -= 0x9E
         end
-        jis_hi += 2*(sjis_hi - 1 - ((sjis_hi <= 0x9F) ? 0x70 : 0xB0))
-        return self.new(jis_hi << 8 | jis_lo)
+        return self.new(sjis_hi, sjis_lo)
       end
     end
 
@@ -58,11 +56,7 @@ module JISX0208
       succ_hi, succ_lo = self.hi, self.lo + 1
       if succ_lo > 0x7E
         succ_lo = 0x21
-        if succ_hi < 0x7E
-          succ_hi += 1
-        else
-          succ_hi = 0x21
-        end
+        succ_hi += 1
       end
       return self.class.new(succ_hi << 8 | succ_lo)
     end
@@ -74,11 +68,15 @@ module JISX0208
       return h << 8 | l
     end
 
+    def inspect
+      "#<JISX0208::Char:#{self.object_id.to_s(16)} sjis=#{self.to_sjis.to_s(16)} jis=#{self.to_int.to_s(16)}>"
+    end
+
     private
 
     def row_cell_to_code(row, cell)
-      unless (1..94).include?(row) && (1..94).include?(cell)
-        raise ArgumentError, "out of row-cell range"
+      unless 0 < row && (1..94).include?(cell)
+        raise ArgumentError, "out of row-cell range: #{row}-#{cell}"
       end
       return (row + 0x20) << 8 | (cell + 0x20)
     end
