f0d1bd14d9dfbdc8c6fd87b366934139d13ed0a1
Kenta Murata <mrkn@mrkn.jp>
2010-02-04 00:26:24 +0900

    add kddi translation table generator and citrus source files

--- tool/test/test_jisx0208.rb
+++ tool/test/test_jisx0208.rb
@@ -10,11 +10,14 @@ class Test_JISX0208_Char < Test::Unit::TestCase
   def test_succ
     assert_equal JISX0208::Char.new(0x2221), JISX0208::Char.new(0x217e).succ
     assert_equal JISX0208::Char.new(2, 1), JISX0208::Char.new(1, 94).succ
-    assert_equal JISX0208::Char.new(0x2121), JISX0208::Char.new(0x7e7e).succ
+    assert_equal JISX0208::Char.new(0x7f21), JISX0208::Char.new(0x7e7e).succ
   end
 
   def test_to_shift_jis
     assert_equal 0x895C, JISX0208::Char.new(0x313D).to_sjis
+    assert_equal 0x895C, JISX0208::Char.from_sjis(0x895C).to_sjis
+    assert_equal 0xF3DE, JISX0208::Char.from_sjis(0xF3DE).to_sjis
+    assert_equal 0xFC40, JISX0208::Char.from_sjis(0xFC40).to_sjis
   end
 
   def test_from_sjis
