2c748b13291cb6848b48b951a6e72da2f35940ed
Kenta Murata <mrkn@mrkn.jp>
2010-02-20 14:38:42 +0900

    added strict encodings

--- enc/x-emoji.c
+++ enc/x-emoji.c
@@ -1,20 +1,38 @@
 #include "regint.h"
 
 ENC_REPLICATE("UTF8-Google", "UTF-8")
-ENC_REPLICATE("UTF8-DoCoMo", "UTF-8")
-ENC_REPLICATE("UTF8-KDDI", "UTF-8")
-ENC_REPLICATE("UTF8-KDDI-UNDOC", "UTF-8")
-ENC_REPLICATE("UTF8-SoftBank", "UTF-8")
-
 ENC_ALIAS("UTF-8-Google", "UTF8-Google")
+
+ENC_REPLICATE("UTF8-DoCoMo_strict", "UTF-8")
+ENC_REPLICATE("UTF8-KDDI_strict", "UTF-8")
+ENC_REPLICATE("UTF8-KDDI-UNDOC_strict", "UTF-8")
+ENC_REPLICATE("UTF8-SoftBank_strict", "UTF-8")
+
+ENC_ALIAS("UTF-8-DoCoMo_strict", "UTF8-DoCoMo_strict")
+ENC_ALIAS("UTF-8-KDDI_strict", "UTF8-KDDI_strict")
+ENC_ALIAS("UTF-8-KDDI-UNDOC_strict", "UTF8-KDDI-UNDOC_strict")
+ENC_ALIAS("UTF-8-SoftBank_strict", "UTF8-SoftBank_strict")
+
+ENC_REPLICATE("Shift_JIS-DoCoMo_strict", "Windows-31J")
+ENC_REPLICATE("Shift_JIS-KDDI_strict", "Windows-31J")
+ENC_REPLICATE("Shift_JIS-SoftBank_strict", "Windows-31J")
+
+ENC_REPLICATE("ISO-2022-JP-KDDI_strict", "ISO-2022-JP")
+ENC_REPLICATE("stateless-ISO-2022-JP-KDDI_strict", "stateless-ISO-2022-JP")
+
+ENC_REPLICATE("UTF8-DoCoMo", "UTF8-DoCoMo_strict")
+ENC_REPLICATE("UTF8-KDDI", "UTF8-KDDI_strict")
+ENC_REPLICATE("UTF8-KDDI-UNDOC", "UTF8-KDDI-UNDOC_strict")
+ENC_REPLICATE("UTF8-SoftBank", "UTF8-SoftBank_strict")
+
 ENC_ALIAS("UTF-8-DoCoMo", "UTF8-DoCoMo")
 ENC_ALIAS("UTF-8-KDDI", "UTF8-KDDI")
 ENC_ALIAS("UTF-8-KDDI-UNDOC", "UTF8-KDDI-UNDOC")
 ENC_ALIAS("UTF-8-SoftBank", "UTF8-SoftBank")
 
-ENC_REPLICATE("Shift_JIS-DoCoMo", "Windows-31J")
-ENC_REPLICATE("Shift_JIS-KDDI", "Windows-31J")
-ENC_REPLICATE("Shift_JIS-SoftBank", "Windows-31J")
+ENC_REPLICATE("Shift_JIS-DoCoMo", "Shift_JIS-DoCoMo_strict")
+ENC_REPLICATE("Shift_JIS-KDDI", "Shift_JIS-KDDI_strict")
+ENC_REPLICATE("Shift_JIS-SoftBank", "Shift_JIS-SoftBank_strict")
 
-ENC_REPLICATE("ISO-2022-JP-KDDI", "ISO-2022-JP")
-ENC_REPLICATE("stateless-ISO-2022-JP-KDDI", "stateless-ISO-2022-JP")
+ENC_REPLICATE("ISO-2022-JP-KDDI", "ISO-2022-JP-KDDI_strict")
+ENC_REPLICATE("stateless-ISO-2022-JP-KDDI", "stateless-ISO-2022-JP-KDDI_strict")
