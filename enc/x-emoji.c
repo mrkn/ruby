51202722a011e82f7ea5df39ff25462279102e3b
Kenta Murata <mrkn@mrkn.jp>
2010-03-02 21:19:21 +0900

    * removed encodings which have UTF-8- prefix.

--- enc/x-emoji.c
+++ enc/x-emoji.c
@@ -1,15 +1,10 @@
 #include "regint.h"
 
 ENC_REPLICATE("UTF8-Google", "UTF-8")
-ENC_ALIAS("UTF-8-Google", "UTF8-Google")
 
 ENC_REPLICATE("UTF8-DoCoMo", "UTF-8")
 ENC_REPLICATE("UTF8-KDDI", "UTF-8")
 ENC_REPLICATE("UTF8-SoftBank", "UTF-8")
-
-ENC_ALIAS("UTF-8-DoCoMo", "UTF8-DoCoMo")
-ENC_ALIAS("UTF-8-KDDI", "UTF8-KDDI")
-ENC_ALIAS("UTF-8-SoftBank", "UTF8-SoftBank")
 
 ENC_REPLICATE("Shift_JIS-DoCoMo", "Windows-31J")
 ENC_REPLICATE("Shift_JIS-KDDI", "Windows-31J")
